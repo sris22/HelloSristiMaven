5ef6816c21b79b76cde02a835c52f6834727a9b3
Sristi Purkayastha <sristipurkayastha@gmail.com>
2026-06-22 13:42:55 +0000

    1st commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this is C1 batch student!" );
+    }
+}
